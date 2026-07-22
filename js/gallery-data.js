4dbe458a900c35b01d58297366a836602c21ebf1
hinaki <takoyakiumasika@yahoo.co.jp>
2026-07-22 18:53:21 +0900

    Add files via upload

--- js/gallery-data.js
+++ js/gallery-data.js
@@ -1,6 +1,12 @@
 "use strict";
 
 const galleryData = [
+       {
+        image: "images/blog.13.jpg",
+        title: "笑っている",
+        category: "ASH JOURNEY",
+        year: "2026"
+    },
    {
         image: "images/gallery27.jpg",
         title: "忠犬",
