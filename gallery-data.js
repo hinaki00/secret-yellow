78343ab4cce779459493865b46f3b75a2172c7b9
hinaki <takoyakiumasika@yahoo.co.jp>
2026-07-22 18:55:00 +0900

    Add files via upload

---
+++ gallery-data.js
@@ -0,0 +1,180 @@
+"use strict";
+
+const galleryData = [
+       {
+        image: "images/blog13.jpg",
+        title: "笑っている",
+        category: "ASH JOURNEY",
+        year: "2026"
+    },
+   {
+        image: "images/gallery27.jpg",
+        title: "忠犬",
+        category: "ASH JOURNEY",
+        year: "2026"
+    },
+
+                                                               {
+        image: "images/gallery26.jpg",
+        title: "椎野 五百合",
+        category: "ILLUSTRATION",
+        year: "2021"
+    },
+
+                                                           {
+        image: "images/gallery25.jpg",
+        title: "白咲 桜",
+        category: "ILLUSTRATION",
+        year: "2021"
+    },
+
+                                                       {
+        image: "images/gallery24.jpg",
+        title: "咬谷 響",
+        category: "ILLUSTRATION",
+        year: "2021"
+    },
+
+                                                   {
+        image: "images/gallery23.jpg",
+        title: "凪坂 昴",
+        category: "ILLUSTRATION",
+        year: "2021"
+    },
+                                                       {
+        image: "images/gallery22.jpg",
+        title: "seaside memory",
+        category: "SECRET BLACK",
+        year: "2021"
+    },
+    
+                                                       {
+        image: "images/gallery21.jpg",
+        title: "椎野さんとアスハちゃん",
+        category: "SECRET BLACK",
+        year: "2021"
+    },
+                                                   {
+        image: "images/gallery20.jpg",
+        title: "椎野 五百合",
+        category: "ILLUSTRATION",
+        year: "2021"
+    },
+       
+    
+                                               {
+        image: "images/gallery19.jpg",
+        title: "春だね",
+        category: "ILLUSTRATION",
+        year: "2023"
+    },
+       
+                                           {
+        image: "images/gallery18.jpg",
+        title: "白咲 桜",
+        category: "ILLUSTRATION",
+        year: "2023"
+    },
+                                       {
+        image: "images/gallery17.jpg",
+        title: "白咲 桜",
+        category: "ILLUSTRATION",
+        year: "2025"
+    },
+                                   {
+        image: "images/gallery16.jpg",
+        title: "慶崎くんと白咲さん",
+        category: "ILLUSTRATION",
+        year: "2025"
+    },
+                                   {
+        image: "images/gallery15.jpg",
+        title: "椎野さんとアスハちゃん",
+        category: "SECRET BLACK",
+        year: "2025"
+    },
+                                   {
+        image: "images/gallery14.jpg",
+        title: "海へ行こうよ",
+        category: "SUMMER GIRL BLUE",
+        year: "2024"
+    },
+                               {
+        image: "images/gallery13.jpg",
+        title: "彼ジャージ",
+        category: "ILLUSTRATION",
+        year: "2026"
+    },
+                            {
+        image: "images/gallery12.jpg",
+        title: "終業式とホワイトーデー、春のはじまり",
+        category: "ILLUSTRATION",
+        year: "2023"
+    },
+                        {
+        image: "images/gallery11.jpg",
+        title: "白咲 桜",
+        category: "ILLUSTRATION",
+        year: "2026"
+    },
+                    {
+        image: "images/gallery10.jpg",
+        title: "慶崎くんと白咲さん",
+        category: "SUMMER GIRL BLUE",
+        year: "2026"
+    },
+                {
+        image: "images/gallery09.jpg",
+        title: "鬼丸くんとあかりちゃん",
+        category: "ILLUSTRATION",
+        year: "2019"
+    },
+            {
+        image: "images/gallery08.jpg",
+        title: "応えろ生きてる星",
+        category: "ILLUSTRATION",
+        year: "2019"
+    },
+        {
+        image: "images/gallery07.jpg",
+        title: "慶崎くんと白咲さん",
+        category: "SUMMER GIRL BLUE",
+        year: "2026"
+    },
+    {
+        image: "images/gallery01.jpg",
+        title: "アッシュジャーニー",
+        category: "ASH JOURNEY",
+        year: "2026"
+    },
+    {
+        image: "images/gallery02.jpg",
+        title: "夏が始まる！",
+        category: "ILLUSTRATION",
+        year: "2026"
+    },
+    {
+        image: "images/gallery03.jpg",
+        title: "向日葵",
+        category: "SUMMER GIRL BLUE",
+        year: "2025"
+    },
+    {
+        image: "images/gallery04.jpg",
+        title: "HPB",
+        category: "ILLUSTRATION",
+        year: "2026"
+    },
+    {
+        image: "images/gallery05.jpg",
+        title: "逃避行",
+        category: "ASH JOURNEY",
+        year: "2026"
+    },
+    {
+        image: "images/gallery06.jpg",
+        title: "ふたり",
+        category: "ASH JOURNEY",
+        year: "2026"
+    }
+];
